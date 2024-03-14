e689279f948d81de3448c3186cf9f3846a4eec95
DitchTurtL <ridium@gmail.com>
2024-03-13 21:04:49 -0400

    Adding event processor

--- GitHookz/Program.cs
+++ GitHookz/Program.cs
@@ -1,6 +1,8 @@
 using Discord.Interactions;
 using Discord.WebSocket;
 using GitHookz.Services;
+using Octokit.Webhooks;
+using Octokit.Webhooks.AspNetCore;
 using Serilog;
 using Serilog.Events;
 using System.Text;
@@ -32,53 +34,14 @@ builder.Services
     .AddSingleton<DiscordSocketClient>()
     .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
     .AddSingleton<InteractionHandler>()
+    .AddSingleton<WebhookEventProcessor, EventProcessor>()
     .AddHostedService<HostedClientService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
-app.MapPost("/webhook", async context =>
-{
-    using (StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8))
-    {
-        string body = await reader.ReadToEndAsync();
-        Log.Information(body);
-        Console.WriteLine(body);
-
-        // Parse the JSON body
-        //JObject payload = JObject.Parse(body);
-
-        // Handle the GitHub webhook payload
-        //Console.WriteLine(payload.ToString());
-    }
-    context.Response.StatusCode = StatusCodes.Status200OK;
-});
-
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapGitHubWebhooks("/webhooks");
 
 app.Urls.Add("http://192.168.0.36:5048/");
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ GitHookz/Services/EventProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Octokit.Webhooks;
+using Octokit.Webhooks.Events;
+using Octokit.Webhooks.Events.PullRequest;
+
+namespace GitHookz.Services
+{
+    public class EventProcessor : WebhookEventProcessor
+    {
+        protected override Task ProcessCreateWebhookAsync(WebhookHeaders headers, CreateEvent createEvent)
+        {
+            return base.ProcessCreateWebhookAsync(headers, createEvent);
+        }
+
+        protected override Task ProcessDeleteWebhookAsync(WebhookHeaders headers, DeleteEvent deleteEvent)
+        {
+            return base.ProcessDeleteWebhookAsync(headers, deleteEvent);
+        }
+
+        protected override Task ProcessPullRequestWebhookAsync(WebhookHeaders headers, PullRequestEvent pullRequestEvent, PullRequestAction action)
+        {
+            return base.ProcessPullRequestWebhookAsync(headers, pullRequestEvent, action);
+        }
+
+
+
+    }
+}
