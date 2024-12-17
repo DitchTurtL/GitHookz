f99266cec2d689a258e47de6fb05437bda9b5960
DitchTurtL <ridium@gmail.com>
2024-12-17 03:45:04 +0000

    Adding Vite client

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GitHookz
+
+A Node+Vite app designed to register Github repositories via Discord, receive webhook notifications, and send repo updates to the assigned discord channel
---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import gitHookzLogo from '/GitHookz.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+          <img src={gitHookzLogo} className="logo" alt="GitHookz logo" />
+        
+      </div>
+      <h1>Hello, GitHookz</h1>
+    </>
+  )
+}
+
+export default App
